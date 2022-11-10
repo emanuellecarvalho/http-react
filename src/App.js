589b92050b1cd5d94f9ff2abed8020e5a5910806
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-11-10 15:41:46 -0300

    feat: create custom hook to fetch the data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+  // 4 - custom hook
+import useFetch  from './hooks/useFetch';
 
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+
+  // 4 - custom hook
+  const { data: items } = useFetch(url);
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   // 1 - resgatando dados
-  useEffect(() => {
+/*   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url);
 
@@ -19,7 +25,7 @@ function App() {
     }
 
     fetchData();
-  }, []);
+  }, []); */
 
   // 2 - add de produtos
   const handleSubmit = async (e) => {
@@ -57,7 +63,7 @@ function App() {
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
-        {products.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
